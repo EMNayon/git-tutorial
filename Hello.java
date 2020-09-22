040ce880d7f6edbb8020f0b2d02ae0293f6a1642
nayon <emranmasudnayon.017@gmail.com>
2020-09-22 08:02:53 +0600

    Added a new method called greeting to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(string[] args){
         System.out.println("Vs code is Awesome..!");
+        greeting("Emran Masud");
+    }
+    public static void greeting(String name){
+        System.out.println("Welcome" + name);
     }
 }
