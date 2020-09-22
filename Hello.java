523c938a3dbd9d898e6c68cf8a8bda50d6b86922
nayon <emranmasudnayon.017@gmail.com>
2020-09-22 09:08:57 +0600

    remove greeting and add displaymethod

--- Hello.java
+++ Hello.java
@@ -1,9 +1,10 @@
 public class Hello {
     public static void main(string[] args){
         System.out.println("Vs code is Awesome..!");
-        greeting("Git");
+        display();
+    
     }
-    public static void greeting(String name){
-        System.out.println("Bye Bye" + name);
+    public static void display(){
+        System.out.println("Hello World");
     }
 }
